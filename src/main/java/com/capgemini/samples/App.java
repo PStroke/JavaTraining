e8d07275781145f9375b8321ccdbdb3ec2765b67
Meziani <oussama.meziani@capgemini.com>
2019-09-09 14:15:48 +0200

    adding a new println()

--- src/main/java/com/capgemini/samples/App.java
+++ src/main/java/com/capgemini/samples/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is my maven project for Capgemini !!! " );
+
+        System.out.println( "Ok");
     }
 }
