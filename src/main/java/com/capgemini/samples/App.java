6e14bea8d69ce23cf0187b08ed88900cad41ee3a
PStroke <skypulse13@gmail.com>
2019-09-09 14:48:07 +0200

    It's time to fight

--- src/main/java/com/capgemini/samples/App.java
+++ src/main/java/com/capgemini/samples/App.java
@@ -6,10 +6,19 @@ public class App
     {
         int i;
         for(i = 0 ; i < 100 ; i++)
-            System.out.println("  MUDA  ");
+            System.out.println(" MUDA ");
 
         int d ;
         for(d = 0 ; d < 100;d++)
             System.out.println(" ORA ");
+
+
+        int c;
+        for(c = 0 ; c < 100;c++)
+            if(c%2 == 0)
+                System.out.println(" ORA ");
+            else
+                System.out.println(" MUDA ");
+
     }
 }
