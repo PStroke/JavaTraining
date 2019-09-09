d49e8c08fc181702a490ecf695957985ec963c84
PStroke <skypulse13@gmail.com>
2019-09-09 14:32:58 +0200

    ZA WOOOOORUDO !!!

--- src/main/java/com/capgemini/samples/App.java
+++ src/main/java/com/capgemini/samples/App.java
@@ -7,5 +7,9 @@ public class App
         int i;
         for(i = 0 ; i < 100 ; i++)
             System.out.println("  MUDA  ");
+
+        int d ;
+        for(d = 0 ; d < 100;d++)
+            System.out.println(" ORA ");
     }
 }
