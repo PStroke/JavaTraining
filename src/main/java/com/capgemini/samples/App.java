13abd6ec31f8decf3c2ac68e69c1ec064c2cb519
Meziani <oussama.meziani@capgemini.com>
2019-09-09 14:21:24 +0200

    added int variable called i

--- src/main/java/com/capgemini/samples/App.java
+++ src/main/java/com/capgemini/samples/App.java
@@ -11,5 +11,8 @@ public class App
         System.out.println( "Hello World! This is my maven project for Capgemini !!! " );
 
         System.out.println( "Ok");
+        int i  = 0   ;
+
+        System.out.println(i);
     }
 }
