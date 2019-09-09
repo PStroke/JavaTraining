1e792ad7c293443cf56a00b3c86c9ab72dd69912
Meziani <oussama.meziani@capgemini.com>
2019-09-09 14:07:57 +0200

    first git commit

---
+++ src/main/java/com/capgemini/samples/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is my maven project for Capgemini !!! " );
+    }
+}
