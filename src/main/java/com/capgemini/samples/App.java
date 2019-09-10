b9b060c762316dd5ee6434383626415ec566e6b2
PStroke <skypulse13@gmail.com>
2019-09-10 09:09:08 +0200

    Delete substraction traces

--- src/main/java/com/capgemini/samples/App.java
+++ src/main/java/com/capgemini/samples/App.java
@@ -4,14 +4,7 @@ public class App
 {
     public void main( String[] args )
     {
-        int result = substraction(10, 5);
-        System.out.println(result);
 
-    }
-
-    public int substraction(Integer i , Integer j)
-    {
-        return i - j ;
     }
 
 }
